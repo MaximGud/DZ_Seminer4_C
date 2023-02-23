1a45882c5da119ea1e72f96ee8e6d6078084176d
maxim_gud <mgudoshnikov@mail.ru>
2023-02-23 09:53:06 +0300

    добавлено решение задачи 25

---
+++ Test25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int PowNumber(int number, int degree)
+{
+  int result = Convert.ToInt32(Math.Pow(number, degree));
+  return result;
+}
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {A} в степени {B} равно - {PowNumber(A,B)}");
